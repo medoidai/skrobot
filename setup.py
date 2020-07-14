069e0d247b7f3e203015c22846c997f9194d7226
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@medoid.ai>
2020-07-14 21:09:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
          "Operating System :: OS Independent",
          "License :: OSI Approved :: MIT License",
          "Programming Language :: Python",
+         "Programming Language :: Python :: 3",
          "Programming Language :: Python :: 3.6",
          "Programming Language :: Python :: 3.7",
          "Topic :: Scientific/Engineering",
