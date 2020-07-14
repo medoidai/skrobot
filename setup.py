4551a7cc8293c088482ee3f2747dbc91ceaf5012
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@medoid.ai>
2020-07-14 17:38:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,5 +32,6 @@ setup(
          "Programming Language :: Python :: 3.6",
          "Programming Language :: Python :: 3.7",
          "Topic :: Scientific/Engineering",
-         "Topic :: Scientific/Engineering :: Artificial Intelligence"]
+         "Topic :: Scientific/Engineering :: Artificial Intelligence",
+         "Environment :: Console"]
 )
