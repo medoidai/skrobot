f7880a18a0ed31f7c256abf26aece5dcd5202f7e
Efstathios Chatzikyriakidisgit config --global user.email stathis.chatzikyriakidis@medoid.ai <stathis.chatzikyriakidis@medoid.ai>
2020-07-22 11:19:58 +0300

    Fix task runner to experiment

--- examples/example_titanic_pipeline_with_filtering_feature_selection.py
+++ examples/example_titanic_pipeline_with_filtering_feature_selection.py
@@ -94,10 +94,10 @@ train_results = experiment.run(TrainTask(estimator=pipe,
                                          random_seed=random_seed))
 
 # Run Prediction Task
-predictions = task_runner.run(PredictionTask(estimator=train_results['estimator'],
-                                             data_set_file_path=new_data_set_file_path,
-                                             id_column=id_column,
-                                             prediction_column=label_column))
+predictions = experiment.run(PredictionTask(estimator=train_results['estimator'],
+                                            data_set_file_path=new_data_set_file_path,
+                                            id_column=id_column,
+                                            prediction_column=label_column))
 
 # Print in-memory results
 print(hyperparameters_search_results['best_params'])
--- examples/example_titanic_pipeline_with_model_based_feature_selection.py
+++ examples/example_titanic_pipeline_with_model_based_feature_selection.py
@@ -107,10 +107,10 @@ train_results = experiment.run(TrainTask(estimator=pipe,
                                          random_seed=random_seed))
 
 # Run Prediction Task
-predictions = task_runner.run(PredictionTask(estimator=train_results['estimator'],
-                                             data_set_file_path=new_data_set_file_path,
-                                             id_column=id_column,
-                                             prediction_column=label_column))
+predictions = experiment.run(PredictionTask(estimator=train_results['estimator'],
+                                            data_set_file_path=new_data_set_file_path,
+                                            id_column=id_column,
+                                            prediction_column=label_column))
 
 # Print in-memory results
 print(features_columns)
