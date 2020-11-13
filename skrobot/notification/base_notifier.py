39640aa9f0550d5c5b77c8435e1384a0add05baf
Michalis Chaviaras <michav1510@gmail.com>
2020-11-13 23:00:52 +0200

    comments in BaseNotifier

--- skrobot/notification/base_notifier.py
+++ skrobot/notification/base_notifier.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 
 class BaseNotifier(ABC):
+    """
+    All notifiers inherit from the :class:`.BaseNotifier`. A notifier can be used to send success / failure notifications for tasks execution.
+    """
     @abstractmethod
     def notify(self, message):
         pass
